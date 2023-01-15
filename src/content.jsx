35044dc3232a860f28bc952e6b4d1937873475f5
Kelvin <vokemalosh32@gmail.com>
2023-01-15 14:53:49 +0300

    add favicon

--- src/content.jsx
+++ src/content.jsx
@@ -32,7 +32,7 @@ const Content = () => {
           } else {
             console.log(data.message);
 
-            setErrors(data.message);
+            setErrors("User Not Found");
           }
         }
       });
@@ -86,7 +86,7 @@ const Content = () => {
             />
             <button
               type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 "
+              className=" cursor-pointer text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 "
             >
               Search
             </button>
